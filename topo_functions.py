86d2938b78d67f410869711e4081545692f24ff8
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2024-12-23 10:55:15 +0100

    Update topo_functions.py

--- topo_functions.py
+++ topo_functions.py
@@ -35,6 +35,7 @@ def get_dgm(point_cloud, deg, device):
         if deg >= 1:
             dgm_in_device['dgms_1'] = torch.tensor(dgm['dgms'][1], device=device) if len(dgm['dgms'][1]) > 0 else torch.empty(0, device=device)
             dgm_in_device['gens_1'] = torch.tensor(dgm['gens'][1], device=device) if len(dgm['gens'][1]) > 0 else torch.empty(0, device=device)
+  print("dgm", dgm, "dgm2", dgm_in_device)
   return dgm_in_device
 
 # Euclidean dist for torch tensors:
