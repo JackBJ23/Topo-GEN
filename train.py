3a13c7ff592e4842ef808da5d2186e9a30ede3bf
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2024-12-23 10:10:43 +0100

    Update train.py

--- train.py
+++ train.py
@@ -177,6 +177,7 @@ if __name__ == "__main__":
   torch.manual_seed(args.seed)
   print("Weights", args.topo_weights)
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  print("device", device)
   model0 = VAE(args.n_latent)
   model1 = VAE(args.n_latent)
   model1.load_state_dict(model0.state_dict())
