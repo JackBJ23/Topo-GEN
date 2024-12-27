274b685050f2c0ab154508279d34fec7f75d6dba
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2024-12-27 14:21:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,24 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     install_requires = f.read().splitlines()
 
+try:
+    with open("README.md") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = ""
+
 setup(
     name="topogen",
     version="0.1.0",
     author="Jack B. Jedlicki",
     author_email="jackbj@berkeley.edu",
     description="A package for topological regularizers and utilities",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JackBJ23/Topo-GEN",
     packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
     ],
     python_requires=">=3.6",
     install_requires=install_requires,
