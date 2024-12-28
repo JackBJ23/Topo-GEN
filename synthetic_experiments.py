f1aa064fb45f42b066594e1e09e3d99d9e471535
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2024-12-29 00:24:48 +0100

    Update synthetic_experiments.py

--- synthetic_experiments.py
+++ synthetic_experiments.py
@@ -9,28 +9,30 @@ from plotly import graph_objects as go
 
 from topogen import get_dgm, loss_bottleneck0, loss_bottleneck1, loss_push0, plot_dgm, generate_gif
 
+# Loss function for the point cloud:
 def loss_bottleneck01(point_cloud, dgm_true, device):
   dgm = get_dgm(point_cloud, 1)
   l_topo0, got_loss0 = loss_bottleneck0(point_cloud, dgm, dgm_true, device)
   l_topo1, got_loss1 = loss_bottleneck1(point_cloud, dgm, dgm_true, device)
   if got_loss0==1 or got_loss1==1: return l_topo0 + l_topo1, l_topo0.item() + l_topo1.item()
-  # only if did not get losses from the previous functions:
+  # If did not get losses from the previous functions: use loss_push0, which adds a small perturbation to the point cloud that "pushes" points away from each other
+  # Empirically, this leads, in the following iterations, to obtain losses from the bottleneck functions 
   return loss_push0(point_cloud, dgm), l_topo0.item() + l_topo1.item()
 
-# Function for running a synthetic test with the bottleneck functions. Saves:
+# Function for running a synthetic test with the bottleneck functions. This function saves images of:
 # i) initial true point cloud, initial true persistence diagram, initial learnable point cloud
-# f) final point cloud, final persistence diagram of point cloud, loss evolution, video of the point cloud evolution
+# f) final point cloud, final persistence diagram of point cloud, loss evolution, and a video of the point cloud evolution during training
 def synthetic_test(point_cloud, point_cloud_true, device, num_steps=2000, num_save=50, lr=0.001, test_name="test", x1=-10., x2=40., y1=-40., y2=40.):
-  # plot initial true point cloud:
+  # Plot initial true point cloud:
   fig = go.Figure(plot_point_cloud(point_cloud_true))
   fig.write_image(f'{test_name}_ini_true_pointcloud.png')
-  # plot initial true PD:
+  # Plot its persistence diagram:
   dgm_true = get_dgm(point_cloud_true, 1)
   plot_dgm(dgm_true, f'{test_name}_ini_true_diagram.png')
-  # plot initial learnable point cloud:
+  # Plot initial learnable point cloud:
   fig = go.Figure(plot_point_cloud(point_cloud))
   fig.write_image(f'{test_name}_ini_pointcloud.png')
-  # plot initial PD of the learnable point cloud:
+  # Plot its persistence diagram:
   dgm = get_dgm(point_cloud, 1)
   plot_dgm(dgm, f'{test_name}_ini_diagram.png')
 
@@ -59,18 +61,18 @@ def synthetic_test(point_cloud, point_cloud_true, device, num_steps=2000, num_sa
         print(f"Iteration {i}/{num_steps}, Loss: {lossitem}")
 
   print("Training ended")
-  # save PD of final point cloud:
+  # save persistence diagram of final point cloud:
   plot_dgm(get_dgm(point_clouds[-1], 1), f'{test_name}_final_diagram.png')
-  # save final point cloud:
+  # Save final point cloud:
   fig = go.Figure(plot_point_cloud(point_clouds[-1]))
   fig.write_image(f'{test_name}_final_pointcloud.png')
-  # save loss evolution:
+  # Save loss evolution:
   plt.figure()
   plt.plot(xs, losses)
   plt.xlabel("Iteration")
   plt.ylabel("Loss")
   plt.savefig(f'{test_name}_loss_evolution.png')
-  # save video of evolution of the point cloud:
+  # Save video of evolution of the point cloud:
   generate_gif(point_clouds, test_name, x1, x2, y1, y2)
   print(f"Test {test_name} done!")
 
@@ -92,7 +94,7 @@ def test1(device):
   for i in range(24):
     point_cloud[i+40][0] = random.uniform(-r1, r1)+10
     point_cloud[i+40][1] = random.uniform(-r1, r1)-25
-  synthetic_test(point_cloud, point_cloud_true, device, 300, 50, 0.01, 'test_1') #15000
+  synthetic_test(point_cloud, point_cloud_true, device, 15000, 50, 0.01, 'test_1')
 
 def test2(device):
   point_cloud_true = np.zeros((128,2))
@@ -118,11 +120,11 @@ def test2(device):
   for i in range(34):
     point_cloud[i+10][0] = random.uniform(-r1, r1)+10.
     point_cloud[i+10][1] = random.uniform(-r1, r1)+5.
-  synthetic_test(point_cloud, point_cloud_true, device, 300, 25, 0.05, 'test_2') #2500
+  synthetic_test(point_cloud, point_cloud_true, device, 2500, 25, 0.05, 'test_2')
 
 def test3(device):
   point_cloud_true = tadasets.dsphere(d=1, n=100, noise=0.0) * 5.
-  #initial point cloud: 2 lines with added noise
+  # Initial point cloud: 2 lines with added noise
   point_cloud = np.zeros((64,2))
   r1 = 0.1
   for i in range(32):
@@ -130,7 +132,7 @@ def test3(device):
     point_cloud[i][1] = float(i)*0.7 + random.uniform(-r1, r1)
     point_cloud[i+32][0] = random.uniform(-r1, r1) + 5. + float(i) * 0.2
     point_cloud[i+32][1] = float(i)*0.9 + random.uniform(-r1, r1)
-  synthetic_test(point_cloud, point_cloud_true, device, 300, 50, 0.1, 'test_3') #7500
+  synthetic_test(point_cloud, point_cloud_true, device, 7500, 50, 0.1, 'test_3')
 
 if __name__ == "__main__":
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
