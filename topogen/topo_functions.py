bd32b1c241f11d8c6a8b8a8c0ff787a85154c457
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2024-12-30 00:42:24 +0100

    Update topo_functions.py

--- topogen/topo_functions.py
+++ topogen/topo_functions.py
@@ -314,10 +314,10 @@ def loss_push0(point_cloud, dgm):
     loss = loss - torch.abs(dist(point_cloud[dgm['gens'][0][i][1]], point_cloud[dgm['gens'][0][i][2]]))/2. #dist to diagonal of (0,d) is d/2
   return loss
 
-def topo_losses(points, true_points, topo_weights, deg=1, dgm_true=None, device="cpu", pers0_delta=0.001, pers1_delta=0.001, dsigma0_scale=0.05, dsigma1_scale=0.05,
+def topo_losses(points, true_points, topo_weights, deg=1, dgm=None, dgm_true=None, device="cpu", pers0_delta=0.001, pers1_delta=0.001, dsigma0_scale=0.05, dsigma1_scale=0.05,
                 density_sigma=0.2, density_scale=0.002, density_maxrange=35., density_npoints=30):
     gotloss = 0
-    dgm = get_dgm(points.view(points.size(0), -1), deg)
+    if dgm is None: dgm = get_dgm(points.view(points.size(0), -1), deg)
     if dgm_true is None: dgm_true = get_dgm(true_points.view(true_points.size(0), -1), deg)
     loss = torch.tensor(0., device=device)
     if topo_weights[0] != 0.:
