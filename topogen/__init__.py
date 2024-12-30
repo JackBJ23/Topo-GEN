33a836c207d13cfce1fbed2a78843a7acb448b4c
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2024-12-30 21:45:06 +0100

    Update __init__.py

--- topogen/__init__.py
+++ topogen/__init__.py
@@ -1,7 +1,21 @@
-from .topo_functions import get_dgm, loss_bottleneck0, loss_bottleneck1, loss_persentropy0, loss_persentropy1,
-  loss_dsigma0, loss_dsigma1, loss_density, loss_push0, topo_losses
+from .topo_functions import (
+    get_dgm, 
+    loss_bottleneck0, loss_bottleneck1, 
+    loss_persentropy0, loss_persentropy1,
+    loss_dsigma0, loss_dsigma1, 
+    loss_density, loss_push0, 
+    topo_losses
+)
+from .visualizations import (
+    plot_dgm, 
+    plot_gen_imgs, 
+    generate_gif
+)
 
-from .visualizations import plot_dgm, plot_gen_imgs, generate_gif
-
-__all__ = ["get_dgm", "loss_bottleneck0", "loss_bottleneck1", "loss_persentropy0", "loss_persentropy1",
-  "loss_dsigma0", "loss_dsigma1", "loss_density", "loss_push0", "topo_losses", "plot_dgm", "plot_gen_imgs", "generate_gif"]
+__all__ = [
+    "get_dgm", "loss_bottleneck0", "loss_bottleneck1", 
+    "loss_persentropy0", "loss_persentropy1",
+    "loss_dsigma0", "loss_dsigma1", 
+    "loss_density", "loss_push0", "topo_losses", 
+    "plot_dgm", "plot_gen_imgs", "generate_gif"
+]
