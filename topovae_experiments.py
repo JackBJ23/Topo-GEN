59cc0cd65129238bd51e90836e6de3f60a6e8dac
JackBJ23 <91531374+JackBJ23@users.noreply.github.com>
2025-01-03 12:24:14 +0100

    Update topovae_experiments.py

--- topovae_experiments.py
+++ topovae_experiments.py
@@ -138,7 +138,7 @@ def train(model0, model1, optimizer0, optimizer1, train_loader, val_loader, dgms
           train_losses1_all.append(BCE1.item())
           print("step", batch_idx)
 
-          if batch_idx % args.n_plot == 0: plot_gen_imgs(data.cpu(), recon_batch0.cpu(), recon_batch1.cpu(), epoch, 'train', batch_idx)
+          if batch_idx % args.n_plot == 0: plot_gen_imgs(data.cpu(), recon_batch0.cpu(), recon_batch1.cpu(), epoch, 'train', batch_idx, filename=f'imgs_train_epoch_{epoch}_step_{batch_idx}')
 
       print("End of epoch", epoch)
       # Average of losses over one epoch:
